539d0f24de1ad678350d53ad8d0ff8d1aba82a22
Milici-Ilie <milicicristyan@yahoo.com>
2024-01-11 04:58:10 +0200

    the About Page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa saepe
+        molestias nam excepturi pariatur ad mollitia ratione quod non dolor ut
+        temporibus autem laudantium fugit, consectetur voluptatem cum, animi
+        maxime. Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+        perspiciatis rerum quibusdam excepturi? Ea illo deleniti quia, quas ut
+        velit quam? Sapiente rem quam atque autem molestiae incidunt eaque iure.
+      </p>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, in
+        sapiente! Totam molestias corporis eaque nobis maxime ipsum numquam
+        iusto, alias voluptatum pariatur omnis sit, architecto optio veritatis
+        non repellendus. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Natus repudiandae voluptatum dicta. Commodi saepe recusandae ipsum
+        maiores repellat! Exercitationem explicabo labore totam earum est
+        possimus magnam animi dolores amet ex.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque alias
+        tempora cumque adipisci esse, quaerat repudiandae magni ex odio
+        consectetur quasi fuga dicta modi nam voluptate perferendis placeat
+        molestiae voluptas! Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Magni autem vitae doloribus quibusdam quas. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Perferendis dolorem sed cumque
+        repudiandae harum nam mollitia, a, magnam corporis consequatur voluptas
+        molestias vel illo eveniet nisi sint qui. Facilis, beatae.
+      </p>
+    </div>
+  );
+}
