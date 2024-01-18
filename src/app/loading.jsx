b8c5418506dc19424c6c6ce74ed620a6fd818148
Milici-Ilie <milicicristyan@yahoo.com>
2024-01-18 23:57:56 +0200

    dddd

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+// import React from "react";
 
 export default function loading() {
-  return <div>loading</div>;
+  return (
+    <div className="flex justify-center">
+      <img src="spinner.svg" alt="loading..." />
+    </div>
+  );
 }
